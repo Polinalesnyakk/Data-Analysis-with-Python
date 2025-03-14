86f7c81d536ed762c938a2d653ba83140e925423
Polinalesnyakk <polinaL2002@gmail.com>
2025-03-14 13:15:26 +0300

    Update submission_template01.py

--- Deep Learning/submission_template01.py
+++ Deep Learning/submission_template01.py
@@ -3,13 +3,24 @@ import torch
 from torch import nn
 
 def create_model():
-    # your code here
-    # return model instance (None is just a placeholder)
-
-    return None
+    model = nn.Sequential(
+        nn.Linear(784, 256),  # Первый линейный слой (784 -> 256)
+        nn.ReLU(),            # Функция активации для скрытого слоя
+        nn.Linear(256, 16),   # Второй линейный слой (256 -> 16)
+        nn.ReLU(),            # Функция активации для скрытого слоя
+        nn.Linear(16, 10)     # Последний линейный слой (16 -> 10)
+    )
+    return model
 
 def count_parameters(model):
-    # your code here
-    # return integer number (None is just a placeholder)
-    
-    return None
+    return sum(p.numel() for p in model.parameters())
+
+# Пример с маленькой моделью
+small_model = nn.Linear(128, 256)
+assert count_parameters(small_model) == 128 * 256 + 256, 'Что-то не так, количество параметров неверное'
+
+# Пример с моделью на основе Sequential
+medium_model = nn.Sequential(*[nn.Linear(128, 32, bias=False), nn.ReLU(), nn.Linear(32, 10, bias=False)])
+assert count_parameters(medium_model) == 128 * 32 + 32 * 10, 'Что-то не так, количество параметров неверное'
+
+print("Seems fine!")
